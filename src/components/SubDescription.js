c2f40c78de0e87bddf3991036337f873c09cd64b
riya-2206 <riya@esmagico.in>
2024-06-03 12:59:04 +0530

    ui changes done

--- src/components/SubDescription.js
+++ src/components/SubDescription.js
@@ -1,45 +1,107 @@
 import React, { useEffect, useState } from "react";
 
 function SubProjectDescription({ selectedProject }) {
-  const [description,setDescription] = useState(null)
-  useEffect(()=>{
+  const [description, setDescription] = useState(null);
+  useEffect(() => {
     fetch(`https://api.dev.c4gt.samagra.io/issues/VedantKhairnar/1`, {
-        method: 'GET',
-      })
-      .then(response => {
+      method: "GET",
+    })
+      .then((response) => {
         return response.json();
       })
-      .then(data => {
-        setDescription(()=>data)
+      .then((data) => {
+        setDescription(() => data);
       })
-      .catch(error => {
-        console.log("error",error.message);
+      .catch((error) => {
+        console.log("error", error.message);
       });
-},[])
+  }, []);
   return (
     <div className="container padding-top--md padding-bottom--lg">
-      {description && <div className="row">
-        <div className="col width">
-          <div>
-            <article>
-              <div className="theme-doc-markdown markdown">
-                <header>
-                  <h1>{selectedProject}</h1>
-                </header>
-                <h2>Overview</h2>
-                <p>
-                  {description?.description}
-                </p>
-                <h2>Weekly Tasks and Learnings</h2>
-                <div
-                  style={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "space-between",
-                  }}
+      {description && (
+        <article>
+          <div className="theme-doc-markdown markdown">
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-between",
+                gap: "28px",
+              }}
+            >
+              <div>
+                <header
+                  style={{ display: "flex", gap: "16px", alignItems: "center" }}
                 >
-                  <h2 style={{ fontSize: "32px" }}>Overall Project PR Table</h2>
+                  <div>
+                    <h1>{selectedProject}</h1>
+                  </div>
+                  <div>
+                    <a href={description?.issue_url}>
+                      <img src={"/img/link_icon.svg"} width={32} height={32} />
+                    </a>
+                  </div>
+                </header>
+                <h2 style={{ fontSize: "32px" }}>Overview</h2>
+                <p>{description?.description}</p>
+
+                {/* <iframe
+        src={description?.weekly_goals_html}
+        title="Example Iframe"
+        width="600"
+        height="400"
+        style={{ border: '1px solid black' }}
+        allowFullScreen
+      ></iframe> */}
+              </div>
+              <div
+                className="table-container thin-scrollbar theme-doc-toc-desktop right-side-table"
+              >
+                <div className="progress-bar-container">
                   <div
+                    className="progress-bar"
+                    style={{ width: `${description?.overall_progress}%` }}
+                  >
+                    {description?.overall_progress + "%"}
+                  </div>
+                </div>
+                <table style={{ width: "100%" }}>
+                  <tbody>
+                    <tr>
+                      <th>{description?.contributor_name}</th>
+                    </tr>
+                    <tr>
+                      <th>{description?.name}</th>
+                    </tr>
+                    <tr>
+                      <th>{description?.org_name}</th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <h2>Weekly Tasks and Learnings</h2>
+            <ul>
+              <li>
+                <a href={description?.weekly_goals_html}>
+                  {description?.weekly_goals_html}
+                </a>
+              </li>
+              <li>
+                <a href={description?.weekly_learnings_html}>
+                  {description?.weekly_learnings_html}
+                </a>
+              </li>
+            </ul>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <h2 style={{ fontSize: "32px" }}>Overall Project PR Table</h2>
+              {/* <div
                     style={{
                       display: "flex",
                       justifyContent: "center",
@@ -47,67 +109,33 @@ function SubProjectDescription({ selectedProject }) {
                     }}
                   >
                     <button style={{ padding: "4px 8px" }}>PR Summary</button>
-                  </div>
-                </div>
-                <table style={{ width: "100%", borderCollapse: "collapse" }}>
-                  <thead>
-                    <tr>
-                      <th align="left">PR Name</th>
-                      <th align="left">Link</th>
-                      <th align="left">Status</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {description?.pr_details?.pr_details?.map((tdata,i)=>{
-                      return <tr key={i}>
+                  </div> */}
+            </div>
+            <table style={{ width: "100%", borderCollapse: "collapse" }}>
+              <thead>
+                <tr>
+                  <th align="left">PR Name</th>
+                  <th align="left">Link</th>
+                  <th align="left">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {description?.pr_details?.pr_details?.map((tdata, i) => {
+                  return (
+                    <tr key={i}>
                       <td align="left">{tdata.name}</td>
                       <td align="left">
                         <a href={tdata.link}>{tdata.link}</a>
                       </td>
                       <td align="left">{tdata.status}</td>
                     </tr>
-                    })}
-                    
-                  </tbody>
-                </table>
-              </div>
-            </article>
-          </div>
-        </div>
-        <div className="col col--3">
-          <div
-            className="table-container thin-scrollbar theme-doc-toc-desktop"
-            style={{
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "column",
-              justifyContent: "center",
-            }}
-          >
-            <div className="progress-bar-container">
-              <div className="progress-bar" style={{ width: description?.overall_progress }}>
-                {description?.overall_progress}
-              </div>
-            </div>
-            <table>
-              <tbody>
-                <tr>
-                  <th>{description?.contributor_name}</th>
-                </tr>
-                <tr>
-                  <th>{description?.name}</th>
-                </tr>
-                <tr>
-                  <th>{description?.org_name}</th>
-                </tr>
+                  );
+                })}
               </tbody>
             </table>
-            {/* <div><div>Contributor Name</div>
-        <div>Mentor Name</div>
-        <div>Organisation</div></div> */}
           </div>
-        </div>
-      </div>}
+        </article>
+      )}
     </div>
   );
 }
