e9c90ef28033847a5d55c3747f2a217c291c6e5f
Praveen Kumar Palaboyina <113330666+praveenkumar911@users.noreply.github.com>
2025-08-13 16:07:14 +0530

    Fix/sorting (#970)
    * Dev (#969)
    
    * add workflow to build and push image
    
    * Feature/issues 2025 (#968)
    
    * added build and push workflow
    
    * 1. Added 2025 component for year 2025 Issues.
    2. Made API endpoints Year Compatible
    3. Made Index.js Reusable for coming years with minimum changes.
    
    * 1. Added Dummy data for 2025
    
    * 1. Added labels for 2025 dmp issues
    
    ---------
    
    Co-authored-by: Karan Trehan <karntrehan@gmail.com>
    Co-authored-by: Srijan-SS02 <codeofficialsrijansriv@gmail.com>
    Co-authored-by: jaanbaaz <106968030+jaanbaaz@users.noreply.github.com>
    Co-authored-by: Shreyash <Shreyash.work.fl@gmail.com>
    
    ---------
    
    Co-authored-by: Karun Agarwal <113603846+singhalkarun@users.noreply.github.com>
    Co-authored-by: Karan Trehan <karntrehan@gmail.com>
    Co-authored-by: Srijan-SS02 <codeofficialsrijansriv@gmail.com>
    Co-authored-by: jaanbaaz <106968030+jaanbaaz@users.noreply.github.com>
    Co-authored-by: Shreyash <Shreyash.work.fl@gmail.com>
    
    * fixed sorting issue in 2025-index.js
    
    ---------
    
    Co-authored-by: Shreyash-work-em <shreyash.sahare@esmagico.in>
    Co-authored-by: Karun Agarwal <113603846+singhalkarun@users.noreply.github.com>
    Co-authored-by: Karan Trehan <karntrehan@gmail.com>
    Co-authored-by: Srijan-SS02 <codeofficialsrijansriv@gmail.com>
    Co-authored-by: jaanbaaz <106968030+jaanbaaz@users.noreply.github.com>
    Co-authored-by: Shreyash <Shreyash.work.fl@gmail.com>
    Co-authored-by: praveenkumar911 <praveenkumarpalaboyina@Praveens-MacBook-Air.local>

--- src/pages/docs/2025/index.js
+++ src/pages/docs/2025/index.js
@@ -60,6 +60,8 @@ function C4GT2025() {
           throw new Error(`${data?.error}`);
         } else {
           let issues = data?.issues;
+           // alpha order  
+          issues = issues?.sort((a, b) => a.org_name.localeCompare(b.org_name));
           setIssueData(() => issues);
           if (ifOrg) {
             setCurrentOrgData(() => {
